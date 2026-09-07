7b210d1e8cb9ef3329552dbd529af25dcea13720
lima <omarlima664-max>
2026-09-07 21:07:55 +0300

    second commit

---
+++ Moduuli 2/teht5.py
@@ -0,0 +1,13 @@
+leiviskat = float(input("Anna leiviskät: "))
+naulat = float(input("Anna naulat: "))
+luodit = float(input("Anna luodit: "))
+
+# Muutetaan kaikki luodeiksi
+luodit_yht = leiviskat * 20 * 32 + naulat * 32 + luodit
+
+grammat = luodit_yht * 13.3
+kilot = int(grammat // 1000)
+grammat = grammat % 1000
+
+print(f"Massa on {kilot} kg ja {grammat:.2f} g")
+
--- readme.md
+++ readme.md
@@ -8,7 +8,9 @@
 Tein tehtävät 1 ja 2.
 
 ## Moduuli 2
-
+Tein tehtävät 1–6.
+Tehtävät 1 ja 2 olivat helppoja.
+Tehtävät 3–6 olivat selvästi vaikeampia
 
 
 
